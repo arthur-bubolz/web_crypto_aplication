cbc00fd47b705ad8e3fe0622e7eeef05ea74e656
arthur-bubolz <101576134+arthur-bubolz@users.noreply.github.com>
2023-10-22 18:05:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,14 +11,17 @@ from markupsafe import Markup
 from pycoingecko import CoinGeckoAPI
 import pandas as pd
 
+# Inicializa o aplicativo Flask
 app = Flask(__name__)
 
+# Inicializa o Socket.IO
 socketio = SocketIO(app,async_mode='threading')
 
+# Inicializa a API CoinGecko
 cg = CoinGeckoAPI()
 print(cg.ping())
 
-
+# Rota raiz que lida com solicitações GET e POST
 @app.route('/', methods=['GET', 'POST'])
 def infos():
     if request.method == 'POST':
@@ -38,14 +41,15 @@ def infos():
                                 include_24hr_change=True,
                                 include_last_updated_at=True)
             
-            # Transforme o JSON em um DataFrame
+            # Transformando o JSON em um DataFrame
             df = pd.DataFrame.from_dict(complexPriceRequest, orient='index')
 
-            # Transponha o DataFrame para que as moedas se tornem índices
+            # Transpondo o DataFrame para que as moedas se tornem índices
             df = df.transpose()
 
             return render_template('index.html', selected_coin=coin_selected, tables = df.to_html(escape=False), titles=df.columns.values)
         
+        #tratamento caso o usuário selecionar a opção de obter conversão
         elif form_name == 'Obter Conversão!':
             coin_selected_1_in = request.form['coin_1']
             coin_selected_2_out = request.form['coin_2']
@@ -56,14 +60,14 @@ def infos():
             
             output_value = request_base/request_out
             
-            # Sua tag HTML aqui
+            #tag HTML aqui
             tag_html = f"<div><p>1 unidade de {coin_selected_1_in} compra: {output_value} de {coin_selected_2_out}</p></div>"
 
-            # Marque a tag HTML como segura para renderização
+            # Marcando a tag HTML como segura para renderização
             safe_html = Markup(tag_html)
             
             return render_template('index.html', safe_html=safe_html)            
-    # Se for um GET ou a primeira renderização, apenas exiba a página sem informações.
+    # Se for um GET ou a primeira renderização, apenas exibir a página sem informações.
     return render_template('index.html')
 
 @app.route('/historical_data', methods=['GET', 'POST'])
@@ -74,21 +78,21 @@ def historical_data():
         #currency selecionada
         currency_selc = request.form['currency']
         
-        #tempo seleiconado
+        #tempo selecionado
         time_select = request.form['time_back']
         
-        # Determine a data final como a data atual
+        # Determinar a data final como a data atual
         data_final = datetime.now()
 
-        # Determine a data inicial como 30 dias antes da data final
+        # Determinar a data inicial como 30 dias antes da data final
         data_inicial = data_final - timedelta(days=int(time_select))
 
-        # Converta as datas para timestamps UNIX (tempo em milisegundos)
+        # Converter as datas para timestamps UNIX (tempo em milisegundos)
         data_inicial_timestamp = int(data_inicial.timestamp())
         data_final_timestamp = int(data_final.timestamp())
 
 
-        # Obtenha os dados históricos de preços
+        # Obter os dados históricos de preços
         historico_precos = cg.get_coin_market_chart_range_by_id(
             id=coin_selected,  # Você pode iterar sobre as criptomoedas desejadas
             vs_currency=currency_selc,
@@ -98,12 +102,12 @@ def historical_data():
         )
 
         # O resultado incluirá dados de preço, volume, etc.
-        # Você pode processar os dados conforme necessário
+        # processar os dados conforme necessário
 
         coinDataFrame_2 = pd.DataFrame.from_dict(historico_precos)
         coinDataFrame_2.tail()
 
-        # Função para extrair o segundo valor de uma lista
+        # Funções para extração dos dados diários da cotação da moeda escolhida
         def extrair_segundo_valor(lista):
             return lista[1] if len(lista) > 1 else None
 
@@ -120,9 +124,8 @@ def historical_data():
 
         coinDataFrame_2['time'] = novo_tempo
         coinDataFrame_2 = coinDataFrame_2[::24]
-        #coinDataFrame_2.head() 
         
-        # Crie um gráfico de linha
+        # Criando um gráfico de linha
         plt.figure(figsize=(10, 6))
         plt.plot(coinDataFrame_2['time'], coinDataFrame_2['prices'], linestyle='-', color='b')
         plt.title('Preço em relação ao tempo')
@@ -130,30 +133,31 @@ def historical_data():
         plt.ylabel('Preço')
         plt.grid(True)
         
-        # Converta o gráfico para uma imagem em bytes
+        # Converter o gráfico para uma imagem em bytes
         img = BytesIO()
         plt.savefig(img, format='png')
         img.seek(0)
         
-        # Converta a imagem em base64 para incorporá-la no HTML
+        # Converter a imagem em base64 para incorporá-la no HTML
         img_base64 = base64.b64encode(img.read()).decode()
         
         prediction_df = coinDataFrame_2[['time','prices']]
         novo_nome_colunas = {'time': 'ds', 'prices': 'y'}
         prediction_df = prediction_df.rename(columns=novo_nome_colunas)
-                
+        
+        #Inicialização da API do Prophet        
         m = Prophet()
         m.fit(prediction_df)
         future = m.make_future_dataframe(periods=int(time_select))   
         forecast = m.predict(future)  
         fig1 = m.plot(forecast)
         
-        # Converta o gráfico para uma imagem em bytes
+        # Converter o gráfico para uma imagem em bytes
         img_2 = BytesIO()
         plt.savefig(img_2, format='png')
         img_2.seek(0)
 
-        # Converta a imagem em base64 para incorporá-la no HTML
+        # Converter a imagem em base64 para incorporá-la no HTML
         img_2_base64 = base64.b64encode(img_2.read()).decode()
            
         return render_template('historical_data.html', selected_coin=coin_selected, img_base64=img_base64, img_2=img_2_base64, n_dias=time_select)
